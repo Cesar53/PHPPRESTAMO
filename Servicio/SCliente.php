c6d35eebea677429ed4e7c6c66590f8ae771cac1
Cesar53 <diegocutipadiego@hotmail.com>
2021-04-08 15:02:36 -0500

    Primer Repositorio

---
+++ Servicio/SCliente.php
@@ -0,0 +1,27 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers:Origin,X-Request-With Content-Type,Accept');
+header('Access-Control-Allow-Methods:GET,POST,PUT,DELETE');
+header('content-type: application/json;charset=utf-8');
+include '../controlador/BD/Conexion.php';
+include '../controlador/Dao/DCliente.php';
+
+try{
+
+
+$dcli=new DCliente();
+$tipo=isset($_REQUEST['tipo'])!=0?$_REQUEST['tipo']:"";
+if($tipo=='lista'){
+    $bus=$_REQUEST["txtbus"];
+    $dcli->getlist($bus);
+    echo json_encode($dcli->getArray());
+}
+
+
+$bus=$_REQUEST["txtbus"];
+$dcli->getlist($bus);
+echo json_encode($dcli->getArray());
+
+
+}catch(Exception $e){echo "Error API:".$e->getMessage();}
+?>
